7a4172785ae5b79bd331f9a9a9f3abd9c57ea5e3
efeakil <efe.akil35@gmail.com>
2023-04-05 19:15:21 +0300

    use effect added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -41,16 +41,19 @@ const Login = (props) => {
     isValid: null,
   });
 
+  const {isValid : emailIsValid} = emailState;
+  const {isValid : passwordIsValid} = passwordState;
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("checking form validity");
-      setFormIsValid(emailState.isValid && passwordState.isValid);
+      setFormIsValid(emailIsValid && passwordIsValid);
     }, 500);
     return () => {
       console.log("Cleaned UP!");
       clearTimeout(identifier);
     };
-  }, [emailState.value, passwordState.value]);
+  }, [emailIsValid , passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
